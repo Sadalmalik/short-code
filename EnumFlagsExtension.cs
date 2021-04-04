0eb5f0dd0ac23647032b7b72a520b773935b2b52
Sadalmalik <Kg252010@yandex.ru>
2021-04-04 16:30:10 +0300

    Add new EnumFlagsExtension

---
+++ EnumFlagsExtension.cs
@@ -0,0 +1,45 @@
+using System;
+
+// Улучшенная версия расширения из PermissionsEnum.cs
+// Теперь это generic методы!
+// И они прицепляются ко всем энумам в проекте.
+
+public static class EnumFlagsExtension
+{
+	public static bool HasFlag<T>(this T target, T flag) where T : Enum
+	{
+		long flags = Convert.ToInt64(target);
+		long value = Convert.ToInt64(flag);
+		return (flags & value) == value;
+	}
+
+	public static void SetFlag<T>(this ref T target, T permissions) where T : struct, Enum
+	{
+		long flags = Convert.ToInt64(target);
+		flags  |= Convert.ToInt64(permissions);
+		target =  (T) Enum.ToObject(typeof(T), flags);
+	}
+
+	public static void ClearFlag<T>(this ref T target, T permissions) where T : struct, Enum
+	{
+		long flags = Convert.ToInt64(target);
+		flags  &= ~Convert.ToInt64(permissions);
+		target =  (T) Enum.ToObject(typeof(T), flags);
+	}
+
+	public static void SetFlags<T>(this ref T target, params T[] permissions) where T : struct, Enum
+	{
+		long flags = Convert.ToInt64(target);
+		foreach (var perm in permissions)
+			flags |= Convert.ToInt64(perm);
+		target = (T) Enum.ToObject(typeof(T), flags);
+	}
+
+	public static void ClearFlags<T>(this ref T target, params T[] permissions) where T : struct, Enum
+	{
+		long flags = Convert.ToInt64(target);
+		foreach (var perm in permissions)
+			flags &= ~Convert.ToInt64(perm);
+		target = (T) Enum.ToObject(typeof(T), flags);
+	}
+}
